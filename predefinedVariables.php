73408caec2c96f3559e90ab4b920cdd0ff5c2272
Saiful Raihan <raihansaiful321@gmail.com>
2021-08-08 00:58:26 +0600

    08-08-2021

--- predefinedVariables.php
+++ predefinedVariables.php
@@ -7,6 +7,22 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        /* $_GLOBALS 
+        Contains a reference to every variable which is currently available within 
+        the global scope of the script.
+        */
+        /* $_SERVER 
+        containing information such as headers, paths, and script locations.
+        The entries in this array are created by the web server.
+        */
+        /* $_GET
+        An associative array of variables passed to the current script via the HTTP GET method.
+        */
+        /* POST 
+        An associative array of variables passed to the current script via the HTTP POST method.
+        */
+        
+    ?>
 </body>
 </html>
